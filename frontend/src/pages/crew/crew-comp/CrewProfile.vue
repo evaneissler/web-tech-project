f8c1233b0f180e0a9962c6d31311ce7981fc21b8
Placide <75864037+PlacideNdai@users.noreply.github.com>
2025-04-30 15:35:26 -0500

    user profile

--- frontend/src/pages/crew/crew-comp/CrewProfile.vue
+++ frontend/src/pages/crew/crew-comp/CrewProfile.vue
@@ -1,18 +1,49 @@
 <template>
-    <!-- <div>
-        <h1>{{ myprofile.firstName }} {{ myprofile.lastName }}</h1>
-        <p>{{ myprofile.id }}</p>
-        <p>{{ myprofile.email }}</p>
-        <p>{{ myprofile.phoneNumber }}</p>
-        <p v-for="position in positions" :key="position.id">{{ position.position }}</p>
-    </div> -->
+    <div v-if="!loading">
+    <div class="profile" >
+        <div class="details">
+            <div class="name">
+                <p>name</p> <br>
+                <h3>{{ myprofile.firstName }} {{ myprofile.lastName }}</h3>
+            </div>
+            <div class="id">
+                <p>id</p> <br>
+                <h3>{{ myprofile.id }}</h3>
+            </div>
+            <div class="email">
+                <p>email</p> <br>
+                <h3>{{ myprofile.email }}</h3>
+            </div>
+            <div class="phone">
+                <p>phone</p> <br>
+                <h3>{{ myprofile.phoneNumber }}</h3>
+            </div>
+            <div class="positions">
+                <p>positions</p> <br>
+                <h3 v-for="position in positions" :key="position.id">{{ position }}</h3>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="upcoming-shifts">
+        <div class="title">
+            <h1>Upcoming Shifts</h1>
+        </div>
+    </div>
+</div>
+
+<div class="title" v-else>
+    <h1>Loading... or network error</h1>
+</div>
 </template>
 
 <script setup>
 import crewapi from '@/api/crewapi';
 import { ref } from 'vue';
 const myprofile = ref(null);
-const positions = ref(null);
+const positions = ref([]);
+const loading = ref(true);
 
 const loadMyProfile = async () => {
     try {
@@ -21,6 +52,8 @@ const loadMyProfile = async () => {
         positions.value = profile.data.positions;
     } catch (err) {
         console.log(err);
+    } finally {
+        loading.value = false;
     }
 }
 loadMyProfile();
@@ -29,4 +62,28 @@ loadMyProfile();
 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+    .profile {
+        display: flex;
+        justify-content: center;
+    }
+
+    .details {
+        display: flex;
+        flex-direction: column;
+        background: #f1f1f1;
+        width: 80%;
+        justify-content: center;
+
+    }
+
+    .details > *{
+        display: flex;
+
+        justify-content: space-around;
+    }
+
+    .title{
+        text-align: center;
+    }
+</style>
